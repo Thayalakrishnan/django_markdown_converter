ad4356c2c01cca999761aeceb93ca342acc06c7c
Lawen <lawen.thaya94@gmail.com>
2024-09-13 17:55:00 +1000

    lazy commit

--- notes/just_test.py
+++ notes/just_test.py
@@ -75,200 +75,37 @@ for _ in items:
     #print("item ----------------------")
     print(repr(_["data"]))
 
+def ConvertListIntoItems(source:str=""):
+    items = []
+    
+    lineitempattern = re.compile(r'(?:^\s*?- ).*?(?=^\s*?- )|(?:^\s*- ).*?$\n?', re.MULTILINE | re.DOTALL)
+    line_id_pattern = re.compile(r'^(?P<level>\s*?)(?P<marker>- )(?P<first>.*?\n)(?P<rest>.*?$\n?)?', re.DOTALL)
+    line_id_pattern = re.compile(r'^(?P<level>\s*?)(?P<marker>- )(?P<content>.*?$\n?)?', re.DOTALL)
+    
+    lines = lineitempattern.findall(source)
 
+    for line in lines:
+        match = line_id_pattern.match(line)
+        if match:
+            newline = []
+            current_line = match.groupdict()
+            current_line["level"] = len(current_line["level"])
+            content = current_line["content"].strip().split("\n")
+            content = [_.lstrip() for _ in content]
+            current_line["content"] = "\n".join(content)
+            items.append(list(current_line.values()))
+    return items
 
-
-# %%
-
-md = """- Item 1: line 1.
-  Item 1: line 2.
-- Item 2: line 1.
-  Item 2: line 2.
-
-  Item 2: line 3.
-  - Item 2.1: line 1.
-  - Item 2.2: line 1.
-  - Item 2.3: line 1.
-    - Item 2.3.1: line 1.
-    - Item 2.3.2: line 1.
-    - Item 2.3.3: line 1.
-  - Item 2.4: line 1.
-- Item 3: line 1.
-  - Item 3.1: line 1.
-    Item 3.1: line 2.
-
-    ```python
-    for p in range(3):
-        print(p)
-    ```
-
-    Item 3.1: line 3.
-  - Item 3.2: line 1.
-- Item 4: line 1.
-"""
-
-ref_stack = []
-items = []
-current_level = []
-
-lineitempattern = re.compile(r'(?:^\s*?- ).*?(?=^\s*?- )|(?:^\s*- ).*?$\n?', re.MULTILINE | re.DOTALL)
-line_id_pattern = re.compile(r'^(?P<level>\s*?)(?P<marker>- )(?P<first>.*?\n)(?P<rest>.*?$\n?)?', re.DOTALL)
-line_id_pattern = re.compile(r'^(?P<level>\s*?)(?P<marker>- )(?P<content>.*?$\n?)?', re.DOTALL)
-
-lines = lineitempattern.findall(md)
-
-for line in lines:
-    match = line_id_pattern.match(line)
-    if match:
-        newline = []
-        current_line = match.groupdict()
-        current_line["level"] = len(current_line["level"])
-        content = current_line["content"].strip().split("\n")
-        content = [_.lstrip() for _ in content]
-        current_line["content"] = "\n".join(content)
-        items.append(list(current_line.values()))
-
-for _ in items:
-    print(_)
+def CreateBlockFromItem(item:tuple=()):
+    #level = len(item[0])
+    #content = current_line["content"].strip().split("\n")
+    #content = [_.lstrip() for _ in content]
+    #current_line["content"] = "\n".join(content)
+    return
     
 
-
-
 # %%
-
-mylines = [
-    [0, 'Item 1', []],
-    [0, 'Item 2', []],
-    [2, 'Item 2.1', []],
-    [2, 'Item 2.2', []],
-    [2, 'Item 2.3', []],
-    [4, 'Item 2.3.1', []],
-    [4, 'Item 2.3.2', []],
-    [4, 'Item 2.3.3', []],
-    [0, 'Item 3', []],
-    [2, 'Item 3.1', []],
-    [2, 'Item 3.2', []],
-    [0, 'Item 4', []],
-]
-
-
-mylines = [
-    [0, 'Item 1', []],
-    [2, 'Item 1.1', []],
-    [4, 'Item 1.1.1', []],
-    [6, 'Item 1.1.1.1', []],
-    [8, 'Item 1.1.1.1.1', []],
-    [6, 'Item 1.1.1.2', []],
-    [4, 'Item 1.1.2', []],
-    [6, 'Item 1.1.2.2', []],
-    [4, 'Item 1.1.3', []],
-]
-
-mylines = [
-    [0, 'Item 1', []],
-    [2, 'Item 1.1', []],
-    [4, 'Item 1.1.1', []],
-    [6, 'Item 1.1.1.1', []],
-    [8, 'Item 1.1.1.1.1', []],
-]
-
-mylines = [
-    [0, 'Item 1', []],
-    [0, 'Item 2', []],
-    [0, 'Item 3', []],
-    [0, 'Item 4', []],
-    [0, 'Item 5', []],
-]
-
-# %%
-mylines = [
-    [0, 'Item 1', []],
-    [0, 'Item 2', []],
-    [2, 'Item 2.1', []],
-    [4, 'Item 2.1.1', []],
-    [0, 'Item 3', []],
-    [0, 'Item 4', []],
-    [2, 'Item 4.1', []],
-    [4, 'Item 4.1.1', []],
-    [0, 'Item 5', []],
-]
-
-def GroupLines(items:list=[]):
-    stack = []
-    root = [0, '', []]
-    
-    cur_parent = root
-    cur_parent_arr = root[2]
-    
-    cur_lvl = 0
-    stack.append(root)
-    
-    index = 0
-    max_index = len(items)
-    
-    while index < max_index:
-        item = items[index]
-        #print(f"cur_lvl: {cur_lvl} item: {item[1]}")
-        if item[0] < cur_lvl:
-            #while item[0] < cur_lvl:
-            #    #print(f"current: {cur_lvl} target: {item[0]}")
-            #    #print(stack)
-            #    cur_parent = stack.pop()
-            #    cur_lvl = cur_parent[0]
-            #    cur_parent_arr = cur_parent[2]
-                
-            cur_parent = stack.pop()
-            cur_lvl = cur_parent[0]
-            cur_parent_arr = cur_parent[2]
-            #cur_parent_arr.append(item)
-        elif item[0] > cur_lvl:
-            """
-            if the current items level is bigger than the 
-            the current level, it is nested under the current item
-            """
-            # update level
-            cur_lvl = item[0]
-            
-            # push current level to stack
-            stack.append(cur_parent)
-            
-            # reference new level, which is the previous level
-            cur_parent = cur_parent_arr[-1]
-            cur_parent_arr = cur_parent[2]
-            
-            # add current item as child of new level
-            #cur_parent_arr.append(item)
-            #index+=1
-        else:
-            cur_parent_arr.append(item)
-            index+=1
-    return root[2]
-
-print(GroupLines(mylines))
-
-# %%
-mylines = [
-    {"level": 0, "content": "Item 1", "children": []},
-    {"level": 0, "content": "Item 2", "children": []},
-    {"level": 0, "content": "Item 3", "children": []},
-    {"level": 0, "content": "Item 4", "children": []},
-    {"level": 0, "content": "Item 5", "children": []},
-]
-# %%
-mylines = [
-    {"level": 0, "content": "Item 1", "children": []},
-    {"level": 0, "content": "Item 2", "children": []},
-    {"level": 2, "content": "Item 2.1", "children": []},
-    {"level": 4, "content": "Item 2.1.1", "children": []},
-    {"level": 4, "content": "Item 2.1.2", "children": []},
-    {"level": 2, "content": "Item 2.2", "children": []},
-    {"level": 4, "content": "Item 2.2.1", "children": []},
-    {"level": 4, "content": "Item 2.2.2", "children": []},
-    {"level": 2, "content": "Item 2.3", "children": []},
-    {"level": 0, "content": "Item 3", "children": []},
-]
-
-def GroupLines(items:list=[]):
+def GroupLines(items):
     """
     """
     root = {"level": 0, "content": "root", "children": []}
@@ -280,9 +117,9 @@ def GroupLines(items:list=[]):
     
     index = 0
     max_index = len(items)
-    
+    item = next(items)
     while index < max_index:
-        item = items[index]
+        #item = items[index]
         
         if item["level"] < cur_lvl:
             cur_parent = stack.pop()
@@ -300,6 +137,125 @@ def GroupLines(items:list=[]):
             cur_parent["children"].append(item)
             index+=1
     return root["children"]
+# %%
 
-print(GroupLines(mylines))
+
+
+def ConvertListIntoItemsTwoStep(source:str=""):
+    items = []
+    lineitempattern = re.compile(r'(?:^\s*?- ).*?(?=^\s*?- )|(?:^\s*- ).*?$\n?', re.MULTILINE | re.DOTALL)
+    line_id_pattern = re.compile(r'^(?P<level>\s*?)(?P<marker>- )(?P<content>.*?$\n?)?', re.DOTALL)
+    
+    lines = lineitempattern.findall(source)
+    for line in lines:
+        match = line_id_pattern.match(line)
+        if match:
+            current_line = match.groupdict()
+            #current_line["level"] = len(current_line["level"])
+            #content = current_line["content"].strip().split("\n")
+            #content = [_.lstrip() for _ in content]
+            #current_line["content"] = "\n".join(content)
+            items.append(tuple(current_line.values()))
+    return items
+
+# %%
+
+
+md = """- Item 1: line 1.
+  Item 1: line 2.
+- Item 2: line 1.
+  Item 2: line 2.
+  
+  Item 2: line 3.
+  - Item 2.1: line 1.
+  - Item 2.2: line 1.
+  - Item 2.3: line 1.
+    1. Item 2.3.1: line 1.
+    2. Item 2.3.2: line 1.
+    3. Item 2.3.3: line 1.
+  - Item 2.4: line 1.
+- Item 3: line 1.
+  - Item 3.1: line 1.
+    Item 3.1: line 2.
+    
+    ```python
+    for p in range(3):
+        print(p)
+    ```
+    
+    Item 3.1: line 3.
+  - Item 3.2: line 1.
+- Item 4: line 1."""
+
+# %%
+import re
+
+def FormatItem(item:dict=()):
+    item["level"] = len(item["level"])
+    item["type"] = "ulist" if item["type"] == "- " else "olist"
+    item["children"] = []
+    content = item["content"].strip().split("\n")
+    content = [_.lstrip() for _ in content]
+    item["content"] = "\n".join(content)
+    return item
+
+
+def ConvertListIntoItems(source:str=""):
+    lineitempattern = re.compile(r'(?P<level>^\s*?)(?P<marker>- )(?P<content>(?:.*?(?=^\s*?- ))|(?:.*?$\n?))', re.MULTILINE | re.DOTALL)
+    lineitempattern = re.compile(r'(?P<level>^\s*?)(?P<marker>(?:- )|(?:\d{1,3}\. ))(?P<content>(?:.*?(?=^\s*?- ))|(?:.*?$\n?))', re.MULTILINE | re.DOTALL)
+    lineitempattern = re.compile(r'(?P<level>^\s*?)(?P<type>(?:- )|(?:\d+\. ))(?P<content>(?:.*?(?=^\s*?((- )|(\d+\. ))))|(?:.*?$\n?))', re.MULTILINE | re.DOTALL)
+    return lineitempattern.finditer(source)
+
+def ConvertListIntoItemGenerator(source:str=""):
+    items = ConvertListIntoItems(source)
+    current_item = FormatItem(next(items).groupdict())
+    try:
+        for next_item in items:
+            yield current_item, True
+            current_item = FormatItem(next_item.groupdict())
+    except StopIteration:
+        pass
+    yield current_item, False
+
+
+def ConvertList(source):
+    """
+    """
+    items = ConvertListIntoItemGenerator(source)
+    
+    root = {"level": 0, "content": "root", "children": []}
+    cur_parent = root
+    cur_lvl = root["level"]
+    
+    stack = []
+    stack_level = []
+    
+    item, valid = next(items)
+    
+    while valid:
+       
+        if item["level"] < cur_lvl:
+            cur_parent = stack.pop()
+            cur_lvl = stack_level.pop()
+        elif item["level"] > cur_lvl:
+            """
+            if the current items level is bigger than the 
+            the current level, it is nested under the current item
+            """
+            stack.append(cur_parent)
+            stack_level.append(cur_lvl)
+            cur_lvl = item["level"]
+            cur_parent = cur_parent["children"][-1]
+        else:
+            cur_parent["children"].append(item)
+            item, valid = next(items)
+    return root["children"]
+
+
+#method1 = ConvertListIntoItemsOneStep(md)
+method2 = ConvertList(md)
+
+print(method2)
+print("Done!")
+
 # %%
