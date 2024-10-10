43589f4239c2f7f38044f325367957dfb51ba0b4
Lawen <lawen.thaya94@gmail.com>
2024-10-10 14:28:00 +1100

    lazy commit

--- proto/misc/token.py
+++ proto/misc/token.py
@@ -2,14 +2,21 @@
 
 
 class TokenValue:
-    __slots__ = ['name', 'depth', 'value', 'root', 'root_ptr']
+    __slots__ = ['name', 'depth', 'value', 'root', 'index', 'stack', 'depth_stack', 'base']
     
-    def __init__(self, name:str="", depth:int=0, value:str="",):
+    def __init__(self, name:str="", depth:int=0, value:str="", index:int=0):
         self.name = name
         self.depth = depth
         self.value = value
-        self.root = [self]
-        self.root_ptr = self.root
+        self.index = index
+        self.stack = []
+        self.depth_stack = []
+        #self.root = [self]
+        self.root = []
+        self.base = self.root
+    
+    def __repr__(self):
+        return f"{self.name}-{self.index}"
     
     def __eq__(self, other):
         return self.name == other.name and type(self.value) == type(other.value) 
@@ -24,42 +31,60 @@ class TokenValue:
         if self < other:
             # nest the other in us
             print("nesting")
-            self.root_ptr.append(other.root)
-            self.root_ptr = other.root
-            #self.depth = other.depth
+            self.stack.append(self.root)
+            self.depth_stack.append(self.depth)
+            new_root = [other]
+            self.root.append(new_root)
+            self.root = new_root
+            self.depth = other.depth
             return self
         elif self > other:
             print("de-nesting")
             # nest us in the other
-            other.root_ptr.append(self.root)
-            other.root_ptr = self.root
+            self.root = self.stack.pop()
+            self.depth = self.depth_stack.pop()
+            return self + other
+            #other.root_ptr.append(self.root)
+            #other.root_ptr = self.root
             #other.depth = self.depth
-            return other
+            #return other
         #elif self == other:
         #    self.value = self.value + other.value
         #    del other
         #    return self
         else:
-            self.value = self.value + other.value
-            del other
+            print("equals")
+            #self.value = self.value + other.value
+            self.root.append(other)
+            #del other
             return self
 
 """
 before **in _nested `double nested` content_ between** after
 """
          
-tv1 = TokenValue("text", 1, "this is the start")
-tv2 = TokenValue("inside", 3, " this is outside middle ")
-tv3 = TokenValue("further", 5, " inside middle ")
-tv4 = TokenValue("text", 1, "this is the end")
+tv0 = TokenValue("root", 0, "", 0)
+tv1 = TokenValue("text", 1, "this is the start", 1)
+tv2 = TokenValue("inside", 3, " this is outside middle ", 2)
+tv3 = TokenValue("further", 5, " inside middle ", 3)
+tv4 = TokenValue("text", 1, "this is the end", 4)
+tv5 = TokenValue("inside", 3, " second middle ", 5)
+tv6 = TokenValue("further", 5, " third middle ", 6)
 
-tv1 + tv2 + tv3 + tv4
-#tv1 + tv2
-#tv1 + tv3
-#tv1 + tv4
-print(tv1.root)
-print(tv2.root)
-print(tv3.root)
-print(tv4.root)
+#tv1 + tv2 + tv3 + tv4
+tv0 + tv1
+print(tv0.base)
+tv0 + tv2
+print(tv0.base)
+tv0 + tv3
+print(tv0.base)
+tv0 + tv4
+print(tv0.base)
+tv0 + tv5
+print(tv0.base)
+tv0 + tv6
+print(tv0.base)
+
+print("done")
             
 # %%
