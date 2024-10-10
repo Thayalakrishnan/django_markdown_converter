0eab18bee1f711afb9a279e55acf473dc62ccefc
Lawen <lawen.thaya94@gmail.com>
2024-10-10 14:37:25 +1100

    lazy commit

--- proto/misc/token.py
+++ proto/misc/token.py
@@ -1,19 +1,13 @@
 #%%
 
-
 class TokenValue:
-    __slots__ = ['name', 'depth', 'value', 'root', 'index', 'stack', 'depth_stack', 'base']
+    __slots__ = ['name', 'depth', 'value', 'index',]
     
     def __init__(self, name:str="", depth:int=0, value:str="", index:int=0):
         self.name = name
         self.depth = depth
         self.value = value
         self.index = index
-        self.stack = []
-        self.depth_stack = []
-        #self.root = [self]
-        self.root = []
-        self.base = self.root
     
     def __repr__(self):
         return f"{self.name}-{self.index}"
@@ -26,13 +20,46 @@ class TokenValue:
     
     def __lt__(self, other):
         return self.depth < other.depth
-                    
+    
+    #def __add__(self, other):
+    #    if self < other:
+    #        # nest the other in us
+    #        print("nesting")
+    #        self.stack.append((self.root, self.depth))
+    #        new_root = [other]
+    #        self.root.append(new_root)
+    #        self.root = new_root
+    #        self.depth = other.depth
+    #        return self
+    #    elif self > other:
+    #        print("de-nesting")
+    #        # nest us in the other
+    #        self.root, self.depth = self.stack.pop()
+    #        return self + other
+    #    #elif self == other:
+    #    #    self.value = self.value + other.value
+    #    #    del other
+    #    #    return self
+    #    else:
+    #        print("equals")
+    #        self.root.append(other)
+    #        #del other
+    #        return self
+
+class RootValue(TokenValue):
+    __slots__ = ['name', 'depth', 'value', 'root', 'index', 'stack', 'base']
+    
+    def __init__(self):
+        super().__init__(name="root", depth=0, value="", index=0)
+        self.stack = []
+        self.root = []
+        self.base = self.root
+    
     def __add__(self, other):
         if self < other:
             # nest the other in us
             print("nesting")
-            self.stack.append(self.root)
-            self.depth_stack.append(self.depth)
+            self.stack.append((self.root, self.depth))
             new_root = [other]
             self.root.append(new_root)
             self.root = new_root
@@ -41,20 +68,14 @@ class TokenValue:
         elif self > other:
             print("de-nesting")
             # nest us in the other
-            self.root = self.stack.pop()
-            self.depth = self.depth_stack.pop()
+            self.root, self.depth = self.stack.pop()
             return self + other
-            #other.root_ptr.append(self.root)
-            #other.root_ptr = self.root
-            #other.depth = self.depth
-            #return other
         #elif self == other:
         #    self.value = self.value + other.value
         #    del other
         #    return self
         else:
             print("equals")
-            #self.value = self.value + other.value
             self.root.append(other)
             #del other
             return self
@@ -62,8 +83,7 @@ class TokenValue:
 """
 before **in _nested `double nested` content_ between** after
 """
-         
-tv0 = TokenValue("root", 0, "", 0)
+tv0 = RootValue()
 tv1 = TokenValue("text", 1, "this is the start", 1)
 tv2 = TokenValue("inside", 3, " this is outside middle ", 2)
 tv3 = TokenValue("further", 5, " inside middle ", 3)
@@ -83,7 +103,7 @@ print(tv0.base)
 tv0 + tv5
 print(tv0.base)
 tv0 + tv6
-print(tv0.base)
+print(tv0.stack[0])
 
 print("done")
             
