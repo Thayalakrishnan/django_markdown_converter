8af23e1ba51e882ad8afe43899b8332bdc625a37
Lawen <lawen.thaya94@gmail.com>
2024-10-21 18:14:13 +1100

    lazy commit

--- django_markdown_converter/patterns/newdata.py
+++ django_markdown_converter/patterns/newdata.py
@@ -1,7 +1,7 @@
 import re
 
 """
-when we grab a paragraph, make sure to merge the lines so that there are non newline characters 
+when we grab a paragraph, make sure to merge the lines so that there are non newline characters
 inside of a pblock
 
 0 | type
@@ -12,51 +12,55 @@ inside of a pblock
 5 | has Nested
 6 | has Inline Markup
 7 | props: values extracted from the pattern that are merged with the other props
-8 | data: 
+8 | data:
 """
 
 META_PATTERN = {
     "type": "meta",
     "pattern": [
-        ("", r"^---*?\n"),
+        r"^---*?\n",
         ("data", r".*?"),
-        ("", r"^---*?\n^"),
-        ("", r"\n|$"),
+        r"^---*?\n^",
+        r"\n",
     ],
     "flags": {
         "MULTILINE": True,
         "DOTALL": True,
     },
-    "hasNested": False,
-    "hasInlineMarkup": False,
+    "attributes": {
+        "hasNested": False,
+        "hasInlineMarkup": False,
+    },
     "props": [],
     "data": ["data"],
 }
 
 CODE_PATTERN = {
     "type": "code",
-    "check": r'^```.*?^```$',
+    #"check": r'^```.*?^```$',
     #"pattern": r'(?:^```)(?P<language>\S+)?\n(?P<data>(?:^.*?\n)+?)(?:^```\n)',
     "pattern": [
-        ("", r"^```"),
+        r"^```",
         ("language", r"\S+"),
-        ("", r"?\n"),
+        r"?\n",
         ("data", r"(?:^.*?\n)+?"),
-        ("", r"^```\n"),
+        r"^```\n",
     ],
     "flags": {
         "MULTILINE": True,
         "DOTALL": False,
     },
-    "hasNested": False,
-    "hasInlineMarkup": False,
+    "attributes": {
+        "hasNested": False,
+        "hasInlineMarkup": False,
+    },
     "props": ["language"],
     "data": ["data"],
 }
 
 DLIST_PATTERN = {
     "type": "dlist",
-    "check": r'^.+?$\n(?:\: .*$)',
+    #"check": r'^.+?$\n(?:\: .*$)',
     #"pattern": r'(?P<term>^.*?\n)(?P<definition>(?:^\: .*?\n)+)',
     # prefix:  ": "
     "pattern": [
@@ -67,8 +71,10 @@ DLIST_PATTERN = {
         "MULTILINE": True,
         "DOTALL": False,
     },
-    "hasNested": False,
-    "hasInlineMarkup": True,
+    "attributes": {
+        "hasNested": False,
+        "hasInlineMarkup": True,
+    },
     "props": [],
     "data": ["definition", "term"],
 }
@@ -77,17 +83,19 @@ FOOTNOTE_PATTERN = {
     "type": "footnote",
     #"pattern": r'^\[\^(?P<index>.+?)\]:\n(?P<data>(?: {1,}.*?\n)+)',
     "pattern": [
-        ("", r"^\[\^"),
+        r"^\[\^",
         ("index", r".+?"),
-        ("", r"\]:\n"),
+        r"\]:\n",
         ("data", r"(?:^ {1,}.*?\n)+"),
     ],
     "flags": {
         "MULTILINE": True,
         "DOTALL": False,
     },
-    "hasNested": True,
-    "hasInlineMarkup": False,
+    "attributes": {
+        "hasNested": True,
+        "hasInlineMarkup": False,
+    },
     "props": ["index"],
     "data": ["data"],
 }
@@ -96,42 +104,44 @@ ADMONITION_PATTERN = {
     "type": "admonition",
     #"pattern": r'(?:^!!!)(?P<type> \S+)?(?: \"(?P<title>.+?)\")?\n(?P<data>(?:^ {1,}.*?\n)+)',
     "pattern": [
-        ("", r"^!!!"),
+        r"^!!!",
         ("type", r" \S+"),
-        ("", r"?"),
+        r"?",
         ("title", r" \".+?\""),
-        ("", r"?\n"),
+        r"?\n",
         ("data", r"(?:^ {1,}.*?\n)+"),
+        r"\n",
     ],
     "flags": {
         "MULTILINE": True,
         "DOTALL": False,
     },
-    "hasNested": True,
-    "hasInlineMarkup": False,
+    "attributes": {
+        "hasNested": True,
+        "hasInlineMarkup": False,
+    },
     "props": ["type", "title"],
     "data": ["data"],
 }
 
 TABLE_PATTERN = {
     "type": "table",
-    "check": r'(?:^\|.*?\|\s*?$\n?)+',
-    "pattern": r'(?P<header>^\|.*?\|\n)(?P<break>^\|.*?\|\n)(?P<body>(?:^\|.*?\|\n)+)',
+    #"check": r'(?:^\|.*?\|\s*?$\n?)+',
+    #"pattern": r'(?P<header>^\|.*?\|\n)(?P<break>^\|.*?\|\n)(?P<body>(?:^\|.*?\|\n)+)',
     "pattern": [
         ("header", r"^\|.*?\|\n"),
         ("break", r"^\|.*?\|\n"),
         ("body", r"(?:^\|.*?\|\n)+"),
-        ("", r"?"),
-        ("title", r" \".+?\""),
-        ("", r"?\n"),
-        ("data", r"(?:^ {1,}.*?\n)+"),
+        r"\n",
     ],
     "flags": {
         "MULTILINE": True,
         "DOTALL": False,
     },
-    "hasNested": False,
-    "hasInlineMarkup": True,
+    "attributes": {
+        "hasNested": False,
+        "hasInlineMarkup": True,
+    },
     "props": [],
     "data": ["header", "body"],
 }
@@ -140,15 +150,16 @@ HR_PATTERN = {
     "type": "hr",
     #"pattern": r'(?P<data>^[\*\-]{3,})\n',
     "pattern": [
-        ("data", r"^[\*\-]{3,}"),
-        ("", r"\n"),
+        ("data", r"^[\*\-]{3,}\n"),
     ],
     "flags": {
         "MULTILINE": True,
         "DOTALL": False,
     },
-    "hasNested": False,
-    "hasInlineMarkup": False,
+    "attributes": {
+        "hasNested": False,
+        "hasInlineMarkup": False,
+    },
     "props": [],
     "data": ["data"],
 }
@@ -159,52 +170,66 @@ HEADING_PATTERN = {
     "pattern": [
         ("level", r"^\#{1,}"),
         ("data", r".*?"),
-        ("", r"\n"),
+        r"\n",
     ],
     "flags": {
         "MULTILINE": True,
         "DOTALL": False,
     },
-    "hasNested": False,
-    "hasInlineMarkup": True,
+    "attributes": {
+        "hasNested": False,
+        "hasInlineMarkup": True,
+    },
     "props": ["level", "data"],
     "data": ["data"],
 }
 
 IMAGE_PATTERN = {
     "type": "image",
-    "check": r'^\!\[.*?\]\(.*?\)',
+    #"check": r'^\!\[.*?\]\(.*?\)',
     #"pattern": r'^\!\[\s*(?P<alt>.*?)?\s*\]\((?P<data>\S*)\s*(?:\"(?P<title>.*?)\")?\)',
-    "pattern": r'^\!\[(?P<alt>.*?)?\]\((?P<data>\S*)(?: *?\"(?P<title>.*?)\")?\)',
+    #"pattern": r'^\!\[(?P<alt>.*?)?\]\((?P<data>\S*)(?: *?\"(?P<title>.*?)\")?\)',
     "pattern": [
-        ("", r"^\!\["),
+        r"^\!\[",
         ("alt", r".*?"),
-        ("", r"?\]\("),
+        r"?\]\(",
         ("data", r"\S*"),
-        ("", r"(?: *?\""),
+        r"(?: *?\"",
         ("title", r".*?"),
-        ("", r"\")?\)\n"),
+        r"\")?\)\n",
     ],
     "flags": {
         "MULTILINE": True,
         "DOTALL": True,
     },
-    "hasNested": False,
-    "hasInlineMarkup": False,
+    "attributes": {
+        "hasNested": False,
+        "hasInlineMarkup": False,
+    },
     "props": ["alt", "title"],
     "data": ["data"],
 }
 
 SVG_PATTERN = {
     "type": "svg",
-    "check": r'^<svg\s[^>]*>(?:.*?)</svg>',
-    "pattern": r'^<svg\s(?P<attrs>[^>]*)>(?P<data>.*?)</svg>',
+    #"check": r'^<svg\s[^>]*>(?:.*?)</svg>',
+    #"pattern": r'^<svg\s(?P<attrs>[^>]*)>(?P<data>.*?)</svg>',
+    #"pattern": r'^\<svg(?P<attrs>[^>]*)\>(?P<data>.*?)\<\/svg\>\n',
+    "pattern": [
+        r"^<svg",
+        ("attrs", r"[^>]*"),
+        r">",
+        ("data", r".*?"),
+        r"</svg>\n",
+    ],
     "flags": {
         "MULTILINE": True,
         "DOTALL": True,
     },
-    "hasNested": False,
-    "hasInlineMarkup": False,
+    "attributes": {
+        "hasNested": False,
+        "hasInlineMarkup": False,
+    },
     "props": ["attrs"],
     "data": ["data"],
 }
@@ -212,14 +237,19 @@ SVG_PATTERN = {
 ULIST_PATTERN = {
     "type": "ulist",
     "check": r'(?:^ *- +.*$)+',
-    "pattern": r'(?P<level>^\s*?)(?P<marker>(?:- )|(?:\d+\. ))(?P<content>(?:.*?(?=^\s*?((- )|(\d+\. ))))|(?:.*?$\n?))',
+    #"pattern": r'(?P<list>(?P<item>^- .*\n(?:^ .*?\n)*)+)',
+    "pattern": [
+        ("item", r"^- .*\n(?:^ .*?\n)*"),
+        r"+",
+    ],
     "flags": {
         "MULTILINE": True,
         "DOTALL": False,
     },
-    #"hasNested": True,
-    "hasNested": False,
-    "hasInlineMarkup": False,
+    "attributes": {
+        "hasNested": True,
+        "hasInlineMarkup": False,
+    },
     "props": ["level", "marker", "content"],
     "data": [],
 }
@@ -227,43 +257,94 @@ ULIST_PATTERN = {
 OLIST_PATTERN = {
     "type": "olist",
     "check": r'(?:^ *\d+\. +.*$)+',
-    "pattern": r'(?P<level>^\s*?)(?P<marker>(?:- )|(?:\d+\. ))(?P<content>(?:.*?(?=^\s*?((- )|(\d+\. ))))|(?:.*?$\n?))',
+    #"pattern": r'(?P<list>(?P<item>^\d+\. .*\n(?:^ .*?\n)*)+)',
+    "pattern": [
+        ("item", r"^\d+\. .*\n(?:^ .*?\n)*"),
+        r"+",
+    ],
     "flags": {
         "MULTILINE": True,
         "DOTALL": False,
     },
-    #"hasNested": True,
-    "hasNested": False,
-    "hasInlineMarkup": False,
+    "attributes": {
+        "hasNested": False,
+        "hasInlineMarkup": False,
+    },
     "props": ["level", "marker", "content"],
     "data": [],
 }
 
 BLOCKQUOTE_PATTERN = {
     "type": "blockquote",
-    "check": r'(?:^>.*$)+',
+    #"check": r'(?:^>.*$)+',
     #"pattern": r'(?<=^>).*(?:\n|$)',
-    "pattern": r'(?P<data>(?:^>.*?\n)+)',
+    #"pattern": r'(?P<data>(?:^>.*?\n)+)',
+    "pattern": [
+        ("data", r"(?:^>.*?\n)+"),
+        r"\n",
+    ],
     "flags": {
         "MULTILINE": True,
         "DOTALL": False,
     },
-    "hasNested": True,
-    "hasInlineMarkup": False,
+    "attributes": {
+        "hasNested": True,
+        "hasInlineMarkup": False,
+    },
     "props": [],
     "data": ["data"],
 }
 
 PARAGRAPH_PATTERN = {
     "type": "paragraph",
-    "check": r'.*',
-    "pattern": r'(?P<data>.*)',
+    #"check": r'.*',
+    #"pattern": r'(?P<data>.*)',
+    "pattern": [
+        ("data", r"^.+?\n"),
+    ],
     "flags": {
         "MULTILINE": True,
         "DOTALL": True,
     },
-    "hasNested": False,
-    "hasInlineMarkup": True,
+    "attributes": {
+        "hasNested": False,
+        "hasInlineMarkup": True,
+    },
     "props": [],
     "data": ["data"],
+}
+
+EMPTYLINE_PATTERN = {
+    "type": "emptyline",
+    "pattern": [
+        r"^\n",
+    ],
+    "flags": {
+        "MULTILINE": False,
+        "DOTALL": False,
+    },
+    "attributes": {
+        "hasNested": False,
+        "hasInlineMarkup": False,
+    },
+    "props": [],
+    "data": [],
+}
+
+
+NEWLINE_PATTERN = {
+    "type": "newline",
+    "pattern": [
+        r"\n",
+    ],
+    "flags": {
+        "MULTILINE": False,
+        "DOTALL": False,
+    },
+    "attributes": {
+        "hasNested": False,
+        "hasInlineMarkup": False,
+    },
+    "props": [],
+    "data": [],
 }
