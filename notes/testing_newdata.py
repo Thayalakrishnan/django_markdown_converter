d4930e7b818c55f841edf7d8c224a734bec9fed1
Lawen <lawen.thaya94@gmail.com>
2024-10-27 23:08:47 +1100

    lazy commit

--- notes/testing_newdata.py
+++ notes/testing_newdata.py
@@ -49,7 +49,6 @@ class PatternProcessing:
     def __init__(self, pattern_attrs:dict=""):
         self.nested = pattern_attrs.get("nested", False)
         self.inlineMarkup = pattern_attrs.get("inlineMarkup", False)
-        
     
 class Pattern:
     #BLOCK_PATTERN, BLOCK_LOOKUP, PATTERN_LOOKUP
@@ -57,9 +56,12 @@ class Pattern:
     PAT_BLOCK_PATTERN = []
     PAT_BLOCK_LOOKUP = {}
     PAT_PATTERN_LOOKUP = {}
+    PAT_INSTANCE_LOOKUP = {}
     
     def __init__(self, pattern_obj:dict=""):
         self.type = pattern_obj.get("type", {})
+        self.attributes = PatternAttributes(pattern_obj.get("attributes", {}))
+        self.processing = pattern_obj.get("processing", {})
         
         label = pattern_obj.get("type", {})
         flags = flag_setter(pattern_obj.get("flags", {}))
@@ -68,13 +70,10 @@ class Pattern:
         self.pattern_match = create_pattern_for_matching(label, pattern_list, flags) 
         self.pattern_extract = create_pattern_for_extracting(pattern_list, flags) 
         
-        self.attributes = PatternAttributes(pattern_obj.get("attributes", {}))
-        self.processing = ""
-        
         self.PAT_BLOCK_PATTERN.append(self.pattern_match)
         self.PAT_BLOCK_LOOKUP[label] = self.pattern_extract
-        #self.PAT_PATTERN_LOOKUP[label] = self
         self.PAT_PATTERN_LOOKUP[label] = pattern_obj
+        self.PAT_INSTANCE_LOOKUP[label] = self
     
     
     @classmethod
@@ -83,56 +82,39 @@ class Pattern:
         cls.PAT_BLOCK_PATTERN = re.compile(f"(?:{cls.PAT_BLOCK_PATTERN})" + cls.PAT_PROPS_PATTERN)
         #return block_pattern, block_lookup, pattern_lookup
         return cls.PAT_BLOCK_PATTERN, cls.PAT_BLOCK_LOOKUP, cls.PAT_PATTERN_LOOKUP
+    
+    def get_props(self, groupdict:dict={}) -> dict:
+        props = groupdict.get("props", {})
+        if props:
+            props = convert_props(props)
+        return props
+    
+    def get_data(self, groupdict:dict={}) -> dict:
+        match = self.pattern_extract.match(groupdict[self.type])
+        if match:
+            return match.groupdict()
+        return {}
+    
+    def process_data(self, data:dict={}) -> dict:
+        for key, process in self.processing.items():
+            data[key] = process(data[key])
+    
+    def convert(self, groupdict:dict={}) -> dict:
+        props = self.get_props(groupdict)
+        data = self.get_data(groupdict)
         
+        data = self.process_data(data)
+        return {
+            "type": self.type,
+            "props": props,
+            "data": data,
+        }
 
-
-def create_patterns(label:str="", pattern_list:list=[], flags:str=""):
-    pattern_parts_for_extracting = []
-    pattern_parts_for_matching = []
-
-    for p in pattern_list:
-        if isinstance(p, str):
-            pattern_parts_for_extracting.append(p)
-            pattern_parts_for_matching.append(p)
-        else:
-            pattern_parts_for_extracting.append(REGEX_NAMED_GROUP(p[0], p[1]))
-            pattern_parts_for_matching.append(REGEX_GROUP(p[1]))
-    
-    pattern_extracting = "".join(pattern_parts_for_extracting)
-    pattern_extracting = re.compile(REGEX_FLAG_GROUP(flags, pattern_extracting))
-    
-    pattern_matching = "".join(pattern_parts_for_matching)
-    pattern_matching = f"(?P<{label}>(?{flags}:{pattern_matching}))"
-    
-    return pattern_matching, pattern_extracting
 
 def create_block_pattern():
-    
-    built_patterns = []
-    block_lookup = {}
-    pattern_lookup = {}
-
     for _ in PATTERNS:
-        pattern_list = _["pattern"]
-        label = _["type"]
-        flags = flag_setter(_["flags"])
-        
-        ma_pattern, ex_pattern = create_patterns(label, pattern_list, flags)
-        
-        block_lookup[label] = ex_pattern
-        built_patterns.append(ma_pattern)
-        pattern_lookup[label] = _
-        
         Pattern(_)
-    
-    
-    props_pattern = r"(?m:^\{ *?(?P<props>.*?) *?\}\n)?"
-    block_pattern = REGEX_GROUP("|".join(built_patterns))
-    block_pattern = re.compile(f"(?:{block_pattern})" + props_pattern)
-    
-    #return block_pattern, block_lookup, pattern_lookup
     return Pattern.build_lookups()
-
 
 BLOCK_PATTERN, BLOCK_LOOKUP, PATTERN_LOOKUP = create_block_pattern()
 
@@ -153,21 +135,23 @@ def extract_block_and_attrs(groupdict:dict={}) -> dict:
         return None
     
     data = BLOCK_LOOKUP[group].match(groupdict[group]).groupdict()
-    
     pattern = PATTERN_LOOKUP[group]
-    attributes = pattern.get("attributes", {})
     
     # process attributes
+    attributes = pattern.get("attributes", {})
+    
     processing = pattern.get("processing", {})
     #if processing:
     for key, process in processing.items():
         data[key] = process(data[key])
     
-    return {
-        "type": group, 
-        "props": extract_props(groupdict), 
-        "data": data
-    }
+    #return {
+    #    "type": group, 
+    #    "props": extract_props(groupdict), 
+    #    "data": data
+    #}
+    
+    return Pattern.PAT_INSTANCE_LOOKUP[group].convert(groupdict)
 
 
 def run_new_mega_tokenizer_with_attrs(source:str=""):
