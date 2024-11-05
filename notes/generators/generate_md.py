ac8f2faed1d8a08eba4bdcd16b555828c0bddfa3
Lawen <lawen.thaya94@gmail.com>
2024-11-05 23:59:05 +1100

    lazy commit

--- notes/generators/generate_md.py
+++ notes/generators/generate_md.py
@@ -14,6 +14,7 @@ LAM_SPACED_JOIN = lambda x=[]: LAM_JOIN(" ", x)
 SCAFFOLD_JOINLINES = lambda x=[]: "\n".join(x)
 SCAFFOLD_FENCED = lambda o="", x=[],c="": "\n".join([o, *x, c, ""])
 SCAFFOLD_META = lambda x=[]: SCAFFOLD_FENCED("---", x,"---")
+SCAFFOLD_ATTRS = lambda x: LAM_SPACED_JOIN(["{", x,"}"])
 
 LAM_HTML_TAG = lambda x,b: f"<{x}>{b}</{x}>"
 LAM_RANDOM_INT = lambda l,u: random.randint(l,u)
@@ -50,7 +51,7 @@ def generate_list_item(listtype:str="ulist", level:int=0, counter:int=1, content
     delimter = "- " if listtype == "ulist" else f"{counter}. "
     if not content:
         content = LAMGEN_FAKE_WORDS(1,6).capitalize()
-    
+
     return f"{indent}{delimter}{content}"
 
 def generate_list_block(listtype="", num_items:int=1, counter:int=1, indentation_level:int=0):
@@ -59,21 +60,21 @@ def generate_list_block(listtype="", num_items:int=1, counter:int=1, indentation
         items.append(generate_list_item(listtype, indentation_level, counter))
         counter+=1
     return items
-    
+
 def generate_list(state:State=None, listtype=""):
     items = []
     stack = []
     indent_counter = 0
     num_list_blocks = random.randint(1,5)
-    
+
     # initial item
     current_counter = 1
     items.append(generate_list_item(listtype, indent_counter, current_counter))
     current_counter+=1
-    
+
     for item in range(num_list_blocks):
         num_list_items = random.randint(1,3)
-        
+
         if LAMGEN_DECISION():
             # indent
             indent_counter+=1
@@ -84,15 +85,15 @@ def generate_list(state:State=None, listtype=""):
             if indent_counter:
                 indent_counter-=1
                 current_counter = stack.pop()
-                
+
         items.extend(generate_list_block(
-            listtype=listtype, 
-            num_items=num_list_items, 
-            counter=current_counter, 
+            listtype=listtype,
+            num_items=num_list_items,
+            counter=current_counter,
             indentation_level=indent_counter
         ))
         current_counter+=num_list_items
-        
+
     # decide to close list or not
     if LAMGEN_DECISION():
         while len(stack):
@@ -104,28 +105,30 @@ def generate_list(state:State=None, listtype=""):
 def indent_content(content:str="") -> str:
     return "\n".join([INDENT + _ for _ in content.split("\n")])
 
-def generate_sub_content(state:State=None, is_indented=False):
+def generate_sub_content(state:State=None, is_indented:bool=False, content_type:str="blocks" ):
     """
     return words, sentences, text only content or other blocks
+    content_type: words, sentence, sentences, blocks
     """
     print("[generate_sub_content]")
     # if nested depth is too deep, we can return just sentences
     state.current_depth+=1
-    
+    if state.current_depth > 3:
+        content = LAMGEN_FAKE_WORDS(1,5)
     # return words
-    if LAMGEN_DECISION():
-        content = LAMGEN_FAKE_WORDS(1,5)    
+    elif LAMGEN_DECISION() or content_type=="words":
+        content = LAMGEN_FAKE_WORDS(1,5)
     # reutrn sentence
-    elif LAMGEN_DECISION():
+    elif LAMGEN_DECISION() or content_type=="sentence":
         content = generate_sentence(state, LAMGEN_DECISION())
-    # return sentences 
-    elif LAMGEN_DECISION():
+    # return sentences
+    elif LAMGEN_DECISION() or content_type=="sentences":
         content = generate_sentences(state, LAMGEN_DECISION())
     else:
         # return blocks
         num_blocks = LAM_RANDOM_INT(1,5)
         content = generate_markdown_blocks(state, num_blocks=num_blocks, is_nested=False, has_meta=False)
-    
+
     state.current_depth-=1
     if is_indented:
         return indent_content(content)
@@ -159,24 +162,24 @@ def generate_markedup_sentence(state:State=None, sentence_length:int=1):
     stack = []
     words = []
     inline_markup = state.INLINE_MARKUP_LIST
-    
+
     for w in range(sentence_length):
         word = fake.word()
-        
+
         if LAMGEN_DECISION(10):
             state.inline_markup_count+=1
             imarkup = random.choice(inline_markup)
             if imarkup not in stack:
                 stack.append(imarkup)
                 word = imarkup[0] + word
-                
+
         # close_inline_markup
         if len(stack) and LAMGEN_DECISION(90):
             imarkup = stack.pop()
             word = word + imarkup[1]
-                
+
         words.append(word)
-        
+
     while len(stack):
         imarkup = stack.pop()
         words[-1]+=imarkup[1]
@@ -197,6 +200,10 @@ def generate_sentences(state:State=None, has_inline_markup:bool=True):
 
 
 ######################
+def generate_attributes(state:State=None):
+    pairs = [f"{fake.word()}=\"{LAMGEN_FAKE_WORDS(1,5)}\"" for _ in LAM_RANDOM_RANGE(1,5)]
+    joined = LAM_SPACED_JOIN(pairs)
+    return SCAFFOLD_ATTRS(joined)
 
 def generate_meta(state:State=None):
     """
@@ -205,7 +212,7 @@ def generate_meta(state:State=None):
     - might have types later, but for now just single word paired
     with some ofther words
     """
-    pairs = [f"{fake.word()}: {LAMGEN_FAKE_WORDS()}" for _ in range(random.randint(1,5))]
+    pairs = [f"{fake.word()}: {LAMGEN_FAKE_WORDS()}" for _ in LAM_RANDOM_RANGE(1,5)]
     return SCAFFOLD_META(pairs)
 
 def generate_code(state:State=None):
@@ -218,9 +225,9 @@ def generate_code(state:State=None):
     if LAMGEN_DECISION(80):
         language = random.choice(["python", "javascript", "c", "cpp", "assembly", "css", "html"])
         header.append(language)
-        
+
     lines.append(LAM_JOIN("", header))
-    
+
     # blocks
     for _ in LAM_RANDOM_RANGE(1,5):
         # lines
@@ -234,7 +241,7 @@ def generate_code(state:State=None):
 def generate_dlist(state:State=None):
     """
     term: random word or words
-    definition: words, sentences, blocks, inline markup 
+    definition: words, sentences, blocks, inline markup
     """
     lines = []
     lines.append(fake.word().capitalize()) # term
@@ -259,7 +266,7 @@ def generate_footnote(state:State=None):
 
     lines.append(index)
     lines.append(content)
-    
+
     print("[generate_footnote] Done")
     return SCAFFOLD_JOINLINES(lines)
 
@@ -396,12 +403,12 @@ def create_generators():
         (generate_paragraph, 15),
         (generate_ulist, 10),
         (generate_olist, 10),
-        
+
         (generate_image, 8),
         (generate_code, 8),
         (generate_footnote, 5),
         (generate_admonition, 5),
-        
+
         (generate_table, 3),
         (generate_svg, 2),
         (generate_html, 2),
@@ -438,13 +445,26 @@ def create_nested_generators():
 
 
 def return_markdown_blocks_list(state:State=None, generator_funcs:Callable=lambda: None, num_blocks:int=1) -> list:
-    return [random.choice(generator_funcs)(state) for _ in range(num_blocks)]
+    blocks = []
+    for _ in range(num_blocks):
+        block = random.choice(generator_funcs)(state)
+        #blocks.append(block)
+        
+        has_attrs = LAMGEN_DECISION(30)
+        if has_attrs:
+            attrs = generate_attributes(state)
+            #blocks.append(attrs)
+            block = block + "\n" + attrs
+            
+        blocks.append(block)   
+    return blocks
 
 def generate_markdown_blocks(state:State=None, num_blocks:int=1, is_nested:bool=False, has_meta:bool=True):
     generator_funcs = create_generators() if not is_nested else create_nested_generators()
     blocks = []
     if has_meta and not is_nested:
         blocks.append(generate_meta(state))
+    
     blocks.extend(return_markdown_blocks_list(state, generator_funcs, num_blocks))
     return "\n\n".join(blocks)
 
@@ -468,7 +488,7 @@ def run_generators():
 
 def run_generator():
     current_state = State()
-    for i in range(3):
+    for i in range(20):
         print(generate_blockquote(current_state))
         print("\n")
 
@@ -488,5 +508,4 @@ def run_generate_markdown_blocks():
 run_generator()
 #run_get_sentences()
 #run_generators()
-
-# %%
+#%%
